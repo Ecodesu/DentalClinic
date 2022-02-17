8b173416c455ab0ef0951a873169d087b32162e9
hendrix034 <hendrixnagle@yahoo.com>
2022-02-18 01:07:41 +0800

    login update

--- api/login.php
+++ api/login.php
@@ -4,7 +4,7 @@ include 'database.php';
 
 $user_name = $_POST['user_name'];
 $password   = hash('ripemd160', $_POST['password']);
-$sql = "SELECT * FROM tbl_user WHERE user_email = '' AND user_password = '$password' ";
+$sql = "SELECT * FROM tbl_user WHERE user_name = '$user_name' AND user_password = '$password' ";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     echo json_encode(array("status"=>true));
