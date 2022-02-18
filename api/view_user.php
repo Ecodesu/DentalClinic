cfe0164c17c75385672e4bd9d67e47fc1c27a0ac
hendrix034 <hendrixnagle@yahoo.com>
2022-02-18 17:23:31 +0800

    update profile done

---
+++ api/view_user.php
@@ -0,0 +1,21 @@
+<?php
+         
+include 'database.php';
+
+$user_id = $_POST['user_id'];
+
+$sql = "SELECT * FROM tbl_user WHERE user_id = '$user_id'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $json_return = array();
+    while($row = $result->fetch_assoc()){
+        $json_return['data']   = $row;
+    }
+    $json_return['status'] = true;
+    
+    echo json_encode($json_return);
+}else{
+    echo json_encode(array("status"=>false));
+}
+
+?>
