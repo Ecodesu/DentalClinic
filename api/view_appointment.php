e3c17be4a8d80e665af55c8694232e1944999a3f
hendrix034 <hendrixnagle@yahoo.com>
2022-02-21 18:53:48 +0800

    view appointment

---
+++ api/view_appointment.php
@@ -0,0 +1,15 @@
+<?php
+include 'database.php';
+$user_id = $_POST['user_id'];
+$sql = "SELECT * FROM tbl_appointment WHERE user_id = $user_id";
+$result = mysqli_query($conn,$sql);
+$arr = array();
+if(mysqli_num_rows($result) > 0){
+	while($rows = mysqli_fetch_assoc($result)){
+		array_push($arr, $rows);
+	}
+}
+header('Content-type: application/json');
+echo json_encode($arr);
+
+?>
