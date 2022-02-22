01994b442b88a8a4c2a28bdc1b1a41e92b03444a
hendrix034 <hendrixnagle@yahoo.com>
2022-02-22 11:32:36 +0800

    revise registration

--- api/user_register.php
+++ api/user_register.php
@@ -10,7 +10,7 @@
 	// $city=$_POST['city'];
 	$sql = "INSERT INTO tbl_user (user_first_name,user_last_name,user_name,user_contact_number,user_password,user_address,user_email,user_credential) 
 	VALUES 
-    ('$first_name','$last_name','$user_name','$contact','$password','$address','$email','patient')";
+    ('$first_name','$last_name','$user_name','$contact','$password','$address','$email','Personal')";
 	if (mysqli_query($conn, $sql)) {
 		echo json_encode(array("status"=>true));
 	} 
