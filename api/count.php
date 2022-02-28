48927b541d48e8749fb59f6efc12aad575702b4d
hendrix034 <hendrixnagle@yahoo.com>
2022-03-01 01:00:39 +0800

    home page polish

---
+++ api/count.php
@@ -0,0 +1,16 @@
+<?php
+include 'database.php';
+$sql = "SELECT * FROM tbl_user WHERE user_credential = 'patient'";	
+
+
+$result = mysqli_query($conn,$sql);
+$arr = array();
+if(mysqli_num_rows($result) > 0){
+	while($rows = mysqli_fetch_assoc($result)){
+		array_push($arr, $rows);
+	}
+}
+header('Content-type: application/json');
+echo json_encode($arr);
+
+?>
