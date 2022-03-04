29a8db6506595c589e55973031193a6983b5d298
hendrix034 <hendrixnagle@yahoo.com>
2022-03-05 00:10:41 +0800

    Update insert.php

--- api/insert.php
+++ api/insert.php
@@ -1,5 +1,14 @@
 <?php
-
-		echo json_encode(hash('ripemd160', $_POST['password']));
+$to = '+639502438249@txtlocal.co.uk';
+$subject = 'from submission';
+$message = 'hello world';
+$header = 'druxbogs@gmail.com';
+$retval = mail($to,$subject,$message,$header);
+if($retval){
+$json_return['status'] = true;
+}else{
+  $json_return['status'] = false; 
+}
+echo json_encode($json_return);
 	
 ?>
