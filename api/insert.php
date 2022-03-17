9ab7defbec9f0c58d0b5a6b6c4535a172955700c
hendrix034 <hendrixnagle@yahoo.com>
2022-03-17 20:08:34 +0800

    done

--- api/insert.php
+++ api/insert.php
@@ -1,14 +1,5 @@
 <?php
-$to = '+639502438249@vtext.com';
-// $subject = 'from submission';
-$message = 'hello world';
-$header = "From:abc@somedomain.com \r\n";
-$retval = mail($to,'',$message,$header);
-if($retval){
-$json_return['status'] = true;
-}else{
-  $json_return['status'] = false; 
-}
-echo json_encode($json_return);
+$password = hash('ripemd160', $_POST['password']);
+echo json_encode($password);
 	
 ?>
